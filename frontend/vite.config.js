60796d171592f8a814a54d619621ae915c5c068a
Your GitHub Username <your-email@example.com>
2026-04-03 13:14:05 +0300

    RESET: Clean dependencies and updated vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // This forces Vite to look in the right place for axios
+      axios: 'axios',
+    },
+  },
+  build: {
+    // This helps Rollup (the bundler) be less strict
+    commonjsOptions: {
+      include: [/node_modules/],
+    },
+  },
 })
